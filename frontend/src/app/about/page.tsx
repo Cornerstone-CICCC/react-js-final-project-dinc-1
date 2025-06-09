cb5d8ecc529cd934a7e839f9ebff2986b90478c9
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-09 12:07:32 -0700

    add: about page

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,36 @@
+const About = () => {
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="text-2xl font-bold">About</h1>
+        <h2>
+          Your trusted marketplace for buying and selling new & pre-loved items.
+        </h2>
+      </div>
+      <p>
+        At Happy Dinca, we believe that great things deserve a second life — or
+        a new home. Our platform empowers every member to buy, sell, and
+        discover a wide range of items, from brand-new products to well-loved
+        treasures.
+      </p>
+      <div>
+        Why ShopDinca?
+        <ul>
+          <li>Seamless and intuitive shopping experience</li>
+          <li>Secure transactions & community trust</li>
+          <li>buyers and sellers</li>
+          <li>Wide</li>
+          <li>Verified</li>
+        </ul>
+        <p>
+          selection across multiple categories Whether you &apos; re hunting for
+          a bargain, selling items you no longer need, or simply exploring,
+          ShopDinca welcomes you. Join our growing community and enjoy happy
+          shopping!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
