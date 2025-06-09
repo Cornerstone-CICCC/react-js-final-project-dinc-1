de5a1c6beceb0087bc30ec9788eea1cd4536562a
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-09 12:36:26 -0700

    fix: added about, contact us menu in footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full border-t-2 border-black bg-white px-4 py-4 flex flex-col-reverse justify-between md:static md:gap-10 md:py-6 md:items-center md:flex-row">
+    <footer className="fixed bottom-0 left-0 w-full border-t-2 border-black bg-white px-4 py-4 text-sm flex justify-between items-end md:static md:gap-10 md:py-6 md:flex-row">
       <p className="uppercase text-muted-foreground text-sm">
         &copy; {new Date().getFullYear()} DINCT. All rights reserved.
       </p>
+      <nav>
+        <ul className="flex flex-col gap-2 text-right">
+          <li>
+            <Link href="/about">ABOUT</Link>
+          </li>
+          <li>
+            <Link href="/contact">CONTACT US</Link>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
