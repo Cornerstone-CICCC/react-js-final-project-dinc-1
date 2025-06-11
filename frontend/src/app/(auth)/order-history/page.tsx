678fb38179e2ab4680a89ff68909a9f97f7c7233
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-10 20:45:04 -0700

    update: order history page

--- frontend/src/app/(auth)/order-history/page.tsx
+++ frontend/src/app/(auth)/order-history/page.tsx
@@ -1,11 +1,11 @@
-import OrderHistoryList from '@/components/order/order-history-list'
+import OrderHistoryList from '@/components/order/order-history-list';
 
 const OrderHistory = () => {
   return (
-    <div>
+    <div className="min-h-screen p-4 lg:px-20 lg:py-10 bg-gray-50">
       <OrderHistoryList />
     </div>
-  )
-}
+  );
+};
 
-export default OrderHistory
+export default OrderHistory;
--- frontend/src/components/order/order-history-list.tsx
+++ frontend/src/components/order/order-history-list.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import { useOrder } from '@/hooks/useOrder'
+import { useOrder } from '@/hooks/useOrder';
 
 const OrderHistoryList = () => {
   const { orders, isLoading, error } = useOrder();
@@ -10,21 +10,39 @@ const OrderHistoryList = () => {
   if (error || !orders) return <div>Error: {error?.message}</div>;
 
   return (
-    <div>
+    <div className="max-w-3xl mx-auto space-y-7">
       {orders.map((order) => (
         <div key={order._id}>
-          <h2>{order.createdAt}</h2>
-          {order.lineItems.map((item, index) => (
-            <div key={index}>
-              <p>{item.productName}</p>
-              <p>${item.unitAmount}</p>
-              <p>{item.quantity}</p>
-            </div>
-          ))}
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg font-semibold mb-2">
+              {order.createdAt.slice(0, 10)}
+            </h2>
+            <span className="text-sm text-muted-foreground">
+              ORDER: {order._id}
+            </span>
+          </div>
+          <div className="bg-white rounded-2xl shadow-sm border-1 border-gray-200 p-4">
+            {order.lineItems.map((item, index) => (
+              <div
+                key={index}
+                className={index > 0 ? 'border-t p-2 pt-3 mt-3' : 'p-2'}
+              >
+                <div className="flex justify-between">
+                  <p className="text-lg font-semibold truncate">
+                    {item.productName}
+                  </p>
+                  <div className="flex gap-3 items-center">
+                    <span className="text-md">${item.unitAmount}</span>
+                    <span>{item.quantity}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default OrderHistoryList
+export default OrderHistoryList;
